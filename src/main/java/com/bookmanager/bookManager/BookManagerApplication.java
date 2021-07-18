d901b38af35393939dd2d8a75e8e2728b5ac7d0e
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2021-07-18 20:12:30 -0300

    desenvolvimento da estrutura do projeto bookmanager

---
+++ src/main/java/com/bookmanager/bookManager/BookManagerApplication.java
@@ -0,0 +1,23 @@
+package com.bookmanager.bookManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagerApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
